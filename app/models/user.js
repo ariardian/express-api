125bcabf144e240b0337ea9b58ba5d4de03d72bf
Ari Ardiansyah <ary.ardyan@gmail.com>
2020-10-21 17:47:20 +0700

    first commit

---
+++ app/models/user.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      allowNull: false
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+    User.belongsToMany(models.Role, {
+      through: 'user_roles',
+      foreignKey: 'userId'
+    });
+
+    User.hasMany(models.Status, {
+      foreignKey: 'user_id',
+      as: 'statuses',
+    });
+  };
+  return User;
+};
